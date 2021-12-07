3b622de28bcd2a62e412fd532c17ba8cbc2938cb
Justin Moore <JustinTMoore@comcast.net>
2021-12-07 12:31:34 -0500

    Before CardList Dictionary

--- main.py
+++ main.py
@@ -1,26 +1,24 @@
 import pstats
 import cProfile
 
-from src.game.enums.TrainColor import TrainColor
 from src.training.Trainer import Trainer
 
 
-# def main():
-    # trainer.training_step()
+def main():
+    trainer.training_step()
 
 
 if __name__ == "__main__":
-    print(list(TrainColor))
-    # trainer = Trainer()
-    # trainer.load_latest_checkpoint()
-    # trainer.tree.simulate_for_n_turns(2)
-    #
-    # cProfile.run('main()', 'output.dat')
-    #
-    # with open("./output_time.txt", "w") as f:
-    #     p = pstats.Stats("output.dat", stream=f)
-    #     p.sort_stats("time").print_stats()
-    #
-    # with open("./output_calls.txt", "w") as f:
-    #     p = pstats.Stats("output.dat", stream=f)
-    #     p.sort_stats("calls").print_stats()
+    trainer = Trainer()
+    trainer.load_latest_checkpoint()
+    trainer.tree.simulate_for_n_turns(2)
+
+    cProfile.run('main()', 'output.dat')
+
+    with open("./output_time.txt", "w") as f:
+        p = pstats.Stats("output.dat", stream=f)
+        p.sort_stats("time").print_stats()
+
+    with open("./output_calls.txt", "w") as f:
+        p = pstats.Stats("output.dat", stream=f)
+        p.sort_stats("calls").print_stats()
